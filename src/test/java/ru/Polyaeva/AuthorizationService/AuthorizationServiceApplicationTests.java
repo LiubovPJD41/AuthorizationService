3164968f9cb8da60c8fc320a1f8e2fb614ae01cf
Liubov P. JD-41 <lovely8419@mail.ru>
2024-07-30 23:09:05 +0100

    update

---
+++ src/test/java/ru/Polyaeva/AuthorizationService/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Polyaeva.AuthorizationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
